1e713e0fba866b9faa9c9d4079e310acbfcec9b7
mcfey <maryfeyrer@hanovernorwichschools.org>
2016-02-10 13:45:12 -0500

    Updated from Brython Server: 2/10/2016 1:45:47 PM

--- tutorial3.py
+++ tutorial3.py
@@ -5,7 +5,6 @@ from ggame import LineStyle, Color, Sprite, Sound
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
 
-myapp = App(SCREEN_WIDTH, SCREEN_HEIGHT)
 
 green = Color(0x00ff00, 1)
 black = Color(0, 1)
@@ -13,6 +12,12 @@ noline = LineStyle(0, black)
 bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, noline, green)
 bg = Sprite(bg_asset, (0,0))
 
+
+#Sounds
+pew1_asset = SoundAsset("sounds/pew1.mp3")
+pew1 = Sound(pew1_asset)
+pop_asset = SoundAsset("sounds/reappear.mp3")
+pop = Sound(pop_asset)
 # A ball! This is already in the ggame-tutorials repository
 ball_asset = ImageAsset("images/orb-150545_640.png")
 ball = Sprite(ball_asset, (0, 0))
@@ -21,6 +26,7 @@ ball.scale = 0.1
 # custom attributes
 ball.dir = 1
 ball.go = True
+
 
 def reverse(b):
     b.dir *= -1
@@ -31,4 +37,22 @@ def step():
             ball.x -= ball.dir
             reverse(ball)
 
+# Handle the space key
+def spaceKey(event):
+    ball.go = not ball.go
+
+# Handle the "reverse" key
+def reverseKey(event):
+    reverse(ball)
+
+# Handle the mouse click
+def mouseClick(event):
+    ball.x = event.x
+    ball.y = event.y
+
+myapp.listenKeyEvent('keydown', 'space', spaceKey)
+myapp.listenKeyEvent('keydown', 'r', reverseKey)
+myapp.listenMouseEvent('click', mouseClick)
+
+myapp = App(SCREEN_WIDTH, SCREEN_HEIGHT)
 myapp.run(step)
