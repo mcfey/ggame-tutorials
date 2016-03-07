0a8dee0896d506e059741b350cba5de2b419abc5
mcfey <maryfeyrer@hanovernorwichschools.org>
2016-03-07 14:40:02 -0500

    Updated from Brython Server: 3/7/2016 2:41:20 PM

--- tutorial4.py
+++ tutorial4.py
@@ -14,6 +14,27 @@ class SpaceShip(Sprite):
 
     def __init__(self, position):
         super().__init__(SpaceShip.asset, position)
+        self.vx = 1
+        self.vy = 1
+        self.vr = 0.01
+        self.thrust = 0
+        self.thrustframe = 1
+        SpaceGame.listenKeyEvent("keydown", "space", self.thrustOn)
+        SpaceGame.listenKeyEvent("keyup", "space", self.thrustOff)
+        
+    def step(self):
+        for ship in self.getSpritesbyClass(SpaceShip):
+            ship.step()
+        
+    def step(self):
+        self.x += self.vx
+        self.y += self.vy
+        self.rotation += self.vr
+        
+    def thrustOn(self, event):
+        self.thrust = 1
+    def thrustOff(self, event):
+        self.thrust = 0
 
 class SpaceGame(App):
     """
